40599cffba3952d13440a7a1091665025f3914dd
Ekaterina Korotkova <e.korotkova.2013@yandex.ru>
2022-06-03 21:49:19 +0300

    implemented modern Quick sort

---
+++ Otus.QuickAndMergeSortings.SeventhHomework/Logic/QuickSortModern.cs
@@ -0,0 +1,60 @@
+﻿namespace Otus.QuickAndMergeSortings.SeventhHomework.Logic
+{
+    public class QuickSortModern
+    {
+        private int[] _array;
+
+        public QuickSortModern(int[] array)
+        {
+            _array = array;
+        }
+
+
+        public int[] Run()
+        {
+            QuickSort(0, _array.Length - 1);
+
+            return _array;
+        }
+
+
+        #region Support methods
+
+        private void QuickSort(int l, int r)
+        {
+            if (l >= r)
+                return;
+
+            var pivotIndex = Split(l, r);
+            
+            QuickSort(l, pivotIndex - 1);
+            QuickSort(pivotIndex + 1, r);
+        }
+
+        private int Split(int l, int r)
+        {
+            var pivot = _array[r];
+            var m = l - 1;
+
+            for (var i = l; i <= r; i++)
+            {
+                if (_array[i] <= pivot && i != m)
+                {
+                    m++;
+                    Swap(m, i);
+                }
+            }
+
+            return m;
+        }
+
+        private void Swap(int x, int y)
+        {
+            var tmp = _array[x];
+            _array[x] = _array[y];
+            _array[y] = tmp;
+        }
+
+        #endregion
+    }
+}
